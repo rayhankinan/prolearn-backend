30cca81084644d15606c044165763de39e0dc7e3
Aira Thalca <13520101@std.stei.itb.ac.id>
2023-02-10 21:50:05 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  
+  const options = new DocumentBuilder()
+    .setTitle('ProLearn API')
+    .setDescription('The ProLearn API description')
+    .setVersion('v1')
+    .addTag('ProLearn')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(8080);
 }
 bootstrap();
