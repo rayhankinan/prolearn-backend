c6192a0844b34026bfe7c2f4983d5dfe671afdd9
rayhankinan <rayhankinan@gmail.com>
2023-02-10 23:08:12 +0700

    Fix swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  
+
   const options = new DocumentBuilder()
     .setTitle('ProLearn API')
     .setDescription('The ProLearn API description')
@@ -14,6 +13,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
+
   await app.listen(8080);
 }
+
 bootstrap();
