64b19e454e1bdb79b3a6a4c1b1b94fd660c7bb58
Aira Thalca <13520101@std.stei.itb.ac.id>
2023-02-14 18:43:29 +0700

    refactor repositories to dto

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -3,10 +3,12 @@ import DatabaseModule from '@database/database.module';
 import LoggerModule from '@logger/logger.module';
 import categoryProviders from './providers/category.provider';
 import CategoryService from './services/category.service';
+import CategoryController from './controllers/category.controller';
 
 @Module({
   imports: [DatabaseModule, LoggerModule],
   providers: [...categoryProviders, CategoryService],
+  controllers: [CategoryController],
 })
 class CategoryModule {}
 
