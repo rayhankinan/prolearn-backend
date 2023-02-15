fa5bc3493d42432ee4c01bd4eb861928e552c592
irsyadazka <16520096@mahasiswa.itb.ac.id>
2023-02-15 15:18:20 +0700

    feat: guard (untested at all)

---
+++ src/guard/roles.guard.ts
@@ -0,0 +1,31 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import UserRole from "@user/enum/user-role";
+import { ROLES_KEY } from "./roles.decorator";
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(private reflector: Reflector) {}
+
+    canActivate(context: ExecutionContext): boolean {
+        const reqRoles = this.reflector.getAllAndOverride<UserRole[]>(ROLES_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+        if (!reqRoles) {
+            return true;
+        }
+
+        const { user } = context.switchToHttp().getRequest();
+        const authorization = user.headers.authorization;
+
+        const token = authorization.replace('Bearer ', '');
+        try {
+            const decoded = jwt.verify(token, 'secret');
+            return reqRoles.some((role) => user.roles?.includes(role));
+        } catch (error) {
+            return false;
+        }
+    }
+}
