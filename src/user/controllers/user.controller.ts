e1df11e7edcd785d0f838026a904f6d40eb1a831
irsyadazka <16520096@mahasiswa.itb.ac.id>
2023-02-28 10:31:39 +0700

    feat: register schema, but cant add the role type

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import UserService from '@user/services/user.service';
 import LocalAuthGuard from '@auth/guard/local.guard';
 import AuthRequest from '@auth/interface/auth-request';
 import ResponseObject from '@response/class/response-object';
+import UserRole from '@user/enum/user-role';
 
 @Controller('user')
 class UserController {
@@ -14,6 +15,12 @@ class UserController {
     const token = await this.userService.tokenize(req.user);
 
     return new ResponseObject<string>('Login is successful', token);
+  }
+
+  @Post('register')
+  async register(@Body('username') username: string, @Body('password') password: string) {
+    const role: UserRole = UserRole.STUDENT;
+    return this.userService.register(username, password, role)
   }
 }
 
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
 import UserEntity from '@user/models/user.model';
 import Payload from '@auth/type/payload';
 import CloudLogger from '@logger/class/cloud-logger';
+import * as argon2 from 'argon2';
+import UserRole from '@user/enum/user-role';
 
 @Injectable()
 class UserService {
@@ -17,6 +19,25 @@ class UserService {
 
   async tokenize(payload: Payload) {
     return this.jwtService.sign(payload);
+  }
+
+  async register(username: string, password: string, role: UserRole) {
+    const existingUser = await this.userRepository.findOne({where: { username } });
+    console.log(existingUser);
+
+    if (existingUser) {
+      throw new ConflictException('Username already exists');
+    }
+
+    const hashPassowrd = await argon2.hash(password);
+    const user = this.userRepository.create({ 
+      username: username, 
+      password: hashPassowrd, 
+      role: role
+    });
+    console.log(user);
+
+    return this.userRepository.save(user);
   }
 }
 
