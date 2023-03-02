2cc55e73d684c9380f24070bceb95c1b5fe9c50a
Aira Thalca <13520101@std.stei.itb.ac.id>
2023-03-02 12:12:44 +0700

    fix non-login user cant access course

--- src/course/controllers/course.controller.ts
+++ src/course/controllers/course.controller.ts
@@ -41,8 +41,6 @@ export default class CourseController {
 
   @ApiProperty({ description: 'Fetch Courses' })
   @Get()
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(UserRole.ADMIN, UserRole.STUDENT)
   async fetchCourse(
     @Request() req: AuthRequest,
     @Query() query: FetchCourseDto,
