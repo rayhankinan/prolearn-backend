cfcd3d78f014cccdb4c0d225232508a20a66cac3
rayhankinan <rayhankinan@gmail.com>
2023-03-03 21:28:44 +0700

    Minor change in storage service

--- src/storage/services/storage.service.ts
+++ src/storage/services/storage.service.ts
@@ -1,11 +1,5 @@
-import { PassThrough } from 'stream';
 import { Injectable } from '@nestjs/common';
-import {
-  Bucket,
-  DownloadResponse,
-  MoveResponse,
-  Storage,
-} from '@google-cloud/storage';
+import { Bucket, DownloadResponse, Storage } from '@google-cloud/storage';
 import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
 import CloudLogger from '@logger/class/cloud-logger';
 import StorageType from '@storage/enum/storage-type';
@@ -13,6 +7,7 @@ import AvailableType from '@storage/enum/available-type';
 import storageConfig from '@storage/config/storage.config';
 import UploadPayload from '@storage/payload/upload-payload';
 import DeletePayload from '@storage/payload/delete-payload';
+import RestorePayload from '@storage/payload/restore-payload';
 
 @Injectable()
 class StorageService {
@@ -32,11 +27,13 @@ class StorageService {
     filetype: StorageType,
     content: Express.Multer.File,
   ): Promise<void> {
-    await this.eventEmitter.emitAsync('file.uploaded', {
+    const payload: UploadPayload = {
       filename,
       filetype,
       content,
-    });
+    };
+
+    await this.eventEmitter.emitAsync('file.uploaded', payload);
   }
 
   async download(
@@ -46,23 +43,26 @@ class StorageService {
     const file = this.bucket.file(
       `${AvailableType.AVAILABLE}/${filetype}/${filename}`,
     );
-
     const downloadResponse = await file.download();
+
     return downloadResponse;
   }
 
   async delete(filename: string, filetype: StorageType): Promise<void> {
-    await this.eventEmitter.emitAsync('file.deleted', { filename, filetype });
+    const payload: DeletePayload = { filename, filetype };
+
+    await this.eventEmitter.emitAsync('file.deleted', payload);
   }
 
   async restore(filename: string, filetype: StorageType): Promise<void> {
-    await this.eventEmitter.emitAsync('file.restored', { filename, filetype });
+    const payload: RestorePayload = { filename, filetype };
+
+    await this.eventEmitter.emitAsync('file.restored', payload);
   }
 
   @OnEvent('file.uploaded', { async: true })
   async uploadHandler(payload: UploadPayload) {
     const { filename, filetype, content } = payload;
-
     const file = this.bucket.file(
       `${AvailableType.AVAILABLE}/${filetype}/${filename}`,
     );
@@ -73,7 +73,6 @@ class StorageService {
   @OnEvent('file.deleted', { async: true })
   async deleteHandler(payload: DeletePayload) {
     const { filename, filetype } = payload;
-
     const file = this.bucket.file(
       `${AvailableType.AVAILABLE}/${filetype}/${filename}`,
     );
@@ -82,9 +81,8 @@ class StorageService {
   }
 
   @OnEvent('file.restored', { async: true })
-  async restoreHandler(payload: DeletePayload) {
+  async restoreHandler(payload: RestorePayload) {
     const { filename, filetype } = payload;
-
     const file = this.bucket.file(
       `${AvailableType.NOT_AVAILABLE}/${filetype}/${filename}`,
     );
